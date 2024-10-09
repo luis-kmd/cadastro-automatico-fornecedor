aa1bdde8750c0df98ba1b48ced389ffe98b9124b
luiskmd <49592247+luis-kmd@users.noreply.github.com>
2024-10-09 16:31:30 -0300

    update visual

--- program.py
+++ program.py
@@ -1,7 +1,8 @@
 import sys
-from PySide6.QtWidgets import QApplication, QDialog, QPlainTextEdit, QPushButton, QLineEdit, QMessageBox
-from PySide6.QtGui import QIcon
-from tela import Ui_Dialog
+from PySide6.QtWidgets import QApplication, QDialog, QMessageBox
+from PySide6.QtGui import QIcon, QPixmap
+from PySide6.QtUiTools import QUiLoader
+from tela_ui import Ui_Dialog  # Classe gerada pelo pyside6-uic
 from PySide6.QtCore import Qt
 
 QApplication.setAttribute(Qt.AA_ShareOpenGLContexts)
@@ -14,8 +15,14 @@ class Cadastro(QDialog):
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)  # Configura a UI
 
-        # Definir ícone da janela
-        self.setWindowIcon(QIcon('C:\\Users\\Luis Tvc\\Documents\\Cadastro de parceiro comercial\\KMD.ico'))
+        fixed_width = 414
+        fixed_height = 451
+        self.setFixedSize(fixed_width, fixed_height)
+
+        self.setWindowTitle("KMD - Cadastro de Parceiros")
+
+        self.icon = QIcon('KMD.png')
+        self.setWindowIcon(self.icon)
 
         # Declarar funções dos botões
         self.Botao_cadastro = self.ui.Lancar
@@ -35,6 +42,39 @@ class Cadastro(QDialog):
         # Armazenando o CNPJ da última consulta
         self.cnpj_consultado = ""
 
+        # Alterar o estilo dos botões
+        self.Botao_consulta.setIcon(QIcon('pesquisar.png'))
+        self.Botao_consulta.setStyleSheet("""
+            QPushButton {
+                background-color: #FFFFFF;
+                color: #9e0505;
+                border-radius: 12px;
+                padding: 0px;
+                font-size: 14px;
+                width: 200px;
+                height: 200px;
+            }
+            QPushButton:hover {
+                background-color: #87CEFA;
+            }
+        """)
+
+        self.Botao_cadastro.setIcon(QIcon('play.png'))
+        self.Botao_cadastro.setStyleSheet("""
+            QPushButton {
+                background-color: #4CAF50;
+                color: white;
+                border-radius: 12px;
+                padding: 0px;
+                font-size: 12px;
+                width: 400px;
+                height: 200px;
+            }
+            QPushButton:hover {
+                background-color: #32CD32;
+            }
+        """)
+
     def texto_inserido(self):
         # Sempre desativa o botão de cadastro quando o texto do CNPJ é alterado
         self.Botao_cadastro.setEnabled(False)
@@ -47,18 +87,15 @@ class Cadastro(QDialog):
             self.Botao_consulta.setEnabled(False)
 
     def botao_consulta(self):
-        # Importar as funções de main.py
         from main import consultar_cnpj, tratar_dados
         cnpj = self.Recebe_CNPJ.text()
         dados_cnpj = consultar_cnpj(cnpj)
 
         if dados_cnpj:
-            # Verifica se houve erro na consulta
             if 'error' in dados_cnpj:
                 self.Recebe_detalhes.setPlainText("Erro na consulta ou CNPJ inválido.")
                 self.Botao_cadastro.setEnabled(False)
             else:
-                # Trata os dados do CNPJ
                 dados_empresa = tratar_dados(dados_cnpj)
                 if dados_empresa:
                     formatted_data = self.formatar_dados(dados_empresa)
@@ -67,7 +104,6 @@ class Cadastro(QDialog):
                     self.Botao_cadastro.setEnabled(True)
                     self.cnpj_consultado = cnpj  # Atualiza o CNPJ consultado
         else:
-            # Se não houver dados, exibe uma mensagem
             self.Recebe_detalhes.setPlainText("Nenhum dado retornado.")
             self.Botao_cadastro.setEnabled(False)
 
@@ -84,7 +120,6 @@ class Cadastro(QDialog):
             f"País: {dados_empresa['pais']}\n"
             f"CEP: {dados_empresa['cep']}\n"
         )
-        # Adiciona a data de status se disponível
         return formatted_data
 
     def botao_cadastro(self):
@@ -95,18 +130,21 @@ class Cadastro(QDialog):
         # Exibir mensagem na tela usando QMessageBox
         msg = QMessageBox()
         msg.setWindowTitle("Cadastro")
-        msg.setIcon(QMessageBox.Information)
+
+        # Carregar o ícone personalizado (exemplo: sucess.png)
+        icon_pixmap = QPixmap('sucess.png')  # Certifique-se de que o caminho para a imagem esteja correto
+        msg.setIconPixmap(icon_pixmap)  # Usar a imagem como ícone
+
         msg.setText(resultado)
         msg.exec()
 
         if "sucesso" in resultado:
-            # Limpa os dados após cadastro bem-sucedido
-            self.Recebe_detalhes.clear()  
+            self.Recebe_detalhes.clear()  # Limpa os dados após cadastro bem-sucedido
             self.Botao_cadastro.setEnabled(False)
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = Cadastro()
-    # Exibe a janela
-    window.show()  
+    window.show()
     sys.exit(app.exec())
